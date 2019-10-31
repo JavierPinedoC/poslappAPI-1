faacecc50c52fe9ec90dfe3a1ba7b14b2b9524e3
tspt89 <tspt89@hotmail.com>
2019-10-31 11:00:19 -0600

    Post Roles Operational

--- api/controllers/roles.controller.js
+++ api/controllers/roles.controller.js
@@ -46,8 +46,39 @@ function getRoles(req, res) {
   }
 }
 
+// post
+function addRol(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    try {
+      console.log("params : ");
+      var myRol = req.body;
+      console.log("gamesystems ... " + myRol);
+    return Roles
+      .create({
+        nombreRol: myRol.nombreRol,
+        filtroPrivilegiosRol: myRol.filtroPrivilegiosRol,
+        }, {
+        /* include: [{
+        model: order_detail,
+        as: 'orderdetail'
+        }] */
+        })
+      .then((myRol) => {
+        res.status(201).send(myRol);
+        })
+      .catch((error) => res.status(400).send(error));
+    } catch (error) {
+    console.log("Was an error");
+    controllerHelper.handleErrorResponse(MODULE_NAME, addRol.name, error, res);
+    }
+  }
+
 module.exports = {
     getRoles,
+    addRol,
     GS_CT_ERR_ROLES_NOT_FOUND,
     GS_CT_DELETED_SUCCESSFULLY,
     MODULE_NAME
